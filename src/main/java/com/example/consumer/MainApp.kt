c89b4fb52a0811f16aceb0651ec11fd2064f4810
mmarcosab <marcosab.vinicius@gmail.com>
2023-12-04 08:29:22 -0300

    add aws s3 put and get object methods

---
+++ src/main/java/com/example/consumer/MainApp.kt
@@ -0,0 +1,15 @@
+package com.example.consumer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.kafka.annotation.EnableKafka
+
+@EnableKafka
+@SpringBootApplication
+class MainApp {
+
+    fun main(args: Array<String>) {
+        runApplication<MainApp>(*args)
+    }
+
+}
