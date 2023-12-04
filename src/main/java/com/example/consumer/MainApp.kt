9078994c53942388bf680f7a7a15a994cc2e1bcd
mmarcosab <marcosab.vinicius@gmail.com>
2023-12-04 08:31:44 -0300

    adjustments

--- src/main/java/com/example/consumer/MainApp.kt
+++ src/main/java/com/example/consumer/MainApp.kt
@@ -6,10 +6,8 @@ import org.springframework.kafka.annotation.EnableKafka
 
 @EnableKafka
 @SpringBootApplication
-class MainApp {
+class MainApp
 
-    fun main(args: Array<String>) {
-        runApplication<MainApp>(*args)
-    }
-
+fun main(args: Array<String>) {
+    runApplication<MainApp>(*args)
 }
